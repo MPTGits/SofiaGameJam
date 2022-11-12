ef6e91381d85b295580db8c0e5d7c80f702043a4
teodorstamenov <teodor.m.stamenov@gmail.com>
2022-11-12 18:18:35 +0200

    Add new resources

--- Levels/TestLevel.gd
+++ Levels/TestLevel.gd
@@ -3,6 +3,8 @@ extends Node2D
 func _ready():
 	var terrain = Globals.terrains[0].instance()
 	add_child(terrain)
+	
+
 
 
 
--- Terrains/BaseTerrain.gd
+++ Terrains/BaseTerrain.gd
@@ -6,6 +6,5 @@ func _on_VisibilityNotifier2D_screen_entered():
 	get_parent().add_child(terrain)
 	terrain.global_position.x = $Position2D.global_position.x
 
-
 func _on_DeleteTerrain_screen_exited():
 	queue_free() 
